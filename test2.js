5e339153c07f8fa93426ec4986fc11974759e4a2
satish-kumar-raj <satishkumarrajbhar679@gmail.com>
2023-09-28 21:04:00 +0530

    This is Js file

---
+++ test2.js
@@ -0,0 +1,30 @@
+// const accountId=897065;
+// let AccountName="satish@gmail.com"
+// var accountPassword="1234";
+// accountCity="Jaipur"
+
+// accountName="s@gmail.com"
+// accountPassword="212121"
+// accountCity="Haidarabad"
+// console.table([accountId,accountPassword,accountCity,accountName])
+// console.log(accountCity)
+/*
+Prefer not to use var
+const let :
+const is used to create constant variable
+const not be redeclared or reasigm
+const is block label
+when the const keyword se variable assign then you provide complesary provide a variable
+
+let is a keyword that is used to declare a variable.
+let is reasign the value but not redeclare.
+let variable scope in block scope 
+
+every variable define undefine value.
+*/
+
+{
+    const a=00;
+    console.log(a)
+}
+// console.log(a)
---
+++ test3.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+// alert(3+3) // we are using nodejs,not browser
+console.log(3 // code readability should be high
+    +3);
+
+console.log("hitest")
+
+let name="Satish"
+let age=18
+let isLogged=true
+
+//number => 2 to power 53-1
+
+// bigint
+// string
+//boolean
+//null : standalone value
+//undefined : when any varible will be defined but not assign a value
+// symbol : unique (React)
+//object : 
+
+console.log(typeof NaN)
