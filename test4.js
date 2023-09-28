c16168b950dac2607e4184257cef3c9da1adc85d
satish-kumar-raj <satishkumarrajbhar679@gmail.com>
2023-09-28 21:27:02 +0530

    Conversion of datatype

---
+++ test4.js
@@ -0,0 +1,28 @@
+let score="23";
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInnumber=Number(score)
+console.log(typeof(valueInnumber))
+
+
+// " 33" =>33
+// "3abc" =>NaN
+// undefined =>Nan
+// Null => 0
+// true =>0
+// false=>1
+let isLoggedIn="h"
+
+let booleanvalue=Boolean(isLoggedIn)
+console.log(booleanvalue)
+
+// 1 => true; 0=>false
+// "" => false
+//"Satish"=>true
+
+
+let sum=78;
+
+let stringNumber=String(sum)
+console.log(typeof stringNumber)
